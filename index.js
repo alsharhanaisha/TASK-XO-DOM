b0d53f8c1a35e2e865de8dc042276da20219bd41
alsharhanaisha <alsharhanaisha@gmail.com>
2022-01-12 21:40:31 +0300

    Aisha & Tahrir - DONE

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function winningAlert(winner) {
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -22,11 +22,27 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+let btnClicked = [];
+let counter = true;
+let box;
 function clickButton(index) {
   console.log(`Button number ${index} is clicked`);
-  // Your main code here.
-}
 
+  if (counter) {
+    box = "x";
+    counter = false;
+  } else {
+    box = "y";
+    counter = true;
+  }
+  if (btnClicked.some((btn) => btn === index)) {
+  } else {
+    btnClicked.push(index);
+  }
+  fillButton(index, box);
+  checkWinner();
+}
+console.log(btnClicked);
 /**
  * 👇🏻 BELOW are functions that you CAN use to structure your code properly.
  * It's always a good idea to make a function for every single purpose.
@@ -34,15 +50,24 @@ function clickButton(index) {
  */
 
 // In this function you should check if the player is X or O
-function checkPlayer() {
-  // ....
-}
+function checkPlayer(box) {}
 
 /**
  *
  * checkWinner should be a function that checks
  * who is winning and returns the winner
  */
-// function checkWinner
+function checkWinner() {
+  const winning = {
+    scenario1: [1, 2, 3],
+    scenario2: [4, 5, 6],
+    scenario3: [7, 8, 9],
+    scenario4: [1, 4, 7],
+    scenario5: [2, 5, 8],
+    scenario6: [3, 6, 9],
+    scenario7: [1, 5, 9],
+    scenario8: [3, 5, 7],
+  };
+}
 
 // function restartGame
